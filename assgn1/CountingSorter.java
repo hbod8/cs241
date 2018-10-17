d0c1a1e131bab30c0d5e8bc6bcc96e42044dc6cf
Harry Saliba <salibah@wwu.edu>
2018-10-17 13:54:20 -0700

    Mostly working counting sorter.

--- assgn1/CountingSorter.java
+++ assgn1/CountingSorter.java
@@ -1,8 +1,26 @@
 public class CountingSorter {
 	public CountingSorter(int biggest) {
-		
+		numbers = new int[biggest + 1];
 	}
+	private final int[] numbers;
 	public void sort(int[] value, int[] digit) {
-		
+		for (int i : digit) {
+			numbers[i]++;
+		}
+		//init sorted array
+		int[] output = new int[digit.length];
+		//prefix sum
+		int previousElements = 0, c;
+		for (int i = 0; i < numbers.length; i++) {
+			c = numbers[i];
+			numbers[i] = previousElements;
+			total += c;
+		}
+		//create sorted array
+		int[] output = new int[digit.length];
+		for (int i = 0; i < output.length; i++) {
+			output[numbers[i]] = x;
+			
+		}
 	}
 }
