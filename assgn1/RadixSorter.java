0e57f5e08c041d5b5e89cda783f08177cc7abb51
Harrison Joseph Saliba <salibah@wwu.edu>
2018-10-17 17:19:36 -0700

    Done, finally done.

--- assgn1/RadixSorter.java
+++ assgn1/RadixSorter.java
@@ -1,17 +1,44 @@
+/*
+Jax Rounds and Harry Saliba
+10/17/18
+This is a radix style of sorter.
+*/
+import java.util.Arrays;
 public class RadixSorter {
 	public void sort(int[] list) {
+		CountingSorter sorter = new CountingSorter(9);
+		int numOfDigits = String.valueOf(max(list)).length();
+		int[] digits = new int[list.length];
+		
+		for (int i = 1; i <= numOfDigits; i++) {
+			for (int j = 0; j < list.length; j++) {
+				digits[j] = getDigit(list[j], i);
+			}
+			
+			sorter.sort(list, digits);
+		}
 		
 	}
+	//Returns the max amount of digits that need to be dealt with
 	private int getDigit(int input, int digit) {
-	//digit is the position of the digit from the right of the number.
+	
 		if (digit == 1) {
 			return input % 10;
-		} else {
+		}
+		else {
 			if (input == 0) {
 				return 0;
 			} else {
-				getDigit(input / 10, digit--);
+				return getDigit((input / 10), digit - 1);
 			}
 		}
+	}
+	//Returns the largest number from the list given
+	private int max(int[] list) {
+		int max = 0;
+		for (int i : list) {
+			if (i > max) max = i;
+		}
+		return max;
 	}
 }
