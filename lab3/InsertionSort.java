7a7515f975536560b857c3f5b85775ede9965662
Harrison Joseph Saliba <salibah@wwu.edu>
2018-10-23 09:35:50 -0700

    Formatted files and added javadocs.

--- lab3/InsertionSort.java
+++ lab3/InsertionSort.java
@@ -1,5 +1,27 @@
+/**
+InsertionSort is a simple class that implments insertion sort for testing by the TestInsertionSort class.
+
+@author Harry Saliba
+@since 23-Oct-18
+*/
 public class InsertionSort {
+	/**
+	Returns sorted array in acending order using insertion sort method.
+	
+	@param arr Array to be sorted.
+	@return int[] Sorted array.
+	*/
 	public int[] insertionSort(int[] arr) {
-		return arr;
+		int[] sorted = new int[arr.legth];
+		System.arraycopy(arr, 0, sorted, 0, arr.length);
+		for (int i = 1; i < sorted.length; i++) {
+			int j = i;
+			while (j > 0 && sorted[j-1] > sorted[j]) {
+				int t = sorted[j]
+				sorted[j] = sorted[j-1];
+				sorted[j-1] = t;
+				j--;
+			}
+		}
 	}
 }
---
+++ lab3/TestInsertionSort.java
@@ -0,0 +1,57 @@
+/**
+TestInsertionSort is used to test the effectiveness of InsertionSort.
+
+@author Harry Saliba
+@since 23-Oct-18
+*/
+public class TestInsertionSort {
+	/**
+	Returns a boolean based on weither or not the array is sorted in acending order. O(n).
+	
+	@param arr Array to be checked.
+	@return boolean True if array is sorted in acending order.
+	*/
+	private static boolean isSorted(int[] arr) {
+		
+	}
+	/**
+	Returns a boolean based on weither or not the arrays contain the same elements.
+	
+	@param a Array to be compared.
+	@param b Array to be compared.
+	@return boolean True if both arrays have the same values.
+	*/
+	private static boolean sameElements(int[] a, int[] b) {
+		
+	}
+	/**
+	Takes a set if integers from the command line to be tested with insertion sort.
+	*/
+	private static void testFromConsole() {
+		while (true) {
+			
+		}
+	}
+	/**
+	A five step process for shuffling an array.
+	
+	1. Generates an integer array of length n.
+	2. Randomly shuffles/permutes the array.
+	3. Uses insertion sort to sort the shuffled array.
+	4. Compared the shuffled and sorted array to the orignal.
+	5. Print the outcome.
+	
+	@param n 
+	*/
+	private static void shuffleTest(int n) {
+		
+	}
+	/**
+	Main method to be run from command line and carry out tasks.
+	
+	@param args Commandline inputs.
+	*/
+	public static void main(String[] args) {
+		
+	}
+}
