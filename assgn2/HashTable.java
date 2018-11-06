3b6c94d7a0096c8fcacc29474c041e4fb6f9441b
Harrison Joseph Saliba <salibah@wwu.edu>
2018-11-05 17:31:46 -0800

    Done.

--- assgn2/HashTable.java
+++ assgn2/HashTable.java
@@ -11,9 +11,9 @@ public class HashTable {
 	 * Constructor that creates an empty table
 	*/
 	public HashTable() {
-		System.out.println("number for happiness is: " + hash("happiness"));
-		System.out.println("number for jax is: " + hash("jax"));
-		System.out.println("number for jxa is: " + hash("jxa"));
+		//System.out.println("number for happiness is: " + hash("happiness"));
+		//System.out.println("number for jax is: " + hash("jax"));
+		//System.out.println("number for jxa is: " + hash("jxa"));
 	}
 	/*
 	 * Adds item to hash table.
@@ -21,7 +21,23 @@ public class HashTable {
 	 * @param word String of word to be added.
 	*/
 	public void add(String word) {
-		
+		word = word.toLowerCase();
+		int index = hash(word);
+		boolean found = false;
+		if (table[index] == null) {
+			table[index] = new LinkedList<DataItem>();
+			table[index].add(new DataItem(word));
+		} else {
+			for (int i = 0; i < table[index].size(); i++) {
+				if (table[index].get(i).word.equals(word)) {
+					table[index].get(i).count++;
+					found = true;
+				}
+			}
+			if (!found) {
+				table[index].add(new DataItem(word));
+			}
+		}
 	}
 	/*
 	 * Deletes item from hash table.
@@ -29,27 +45,65 @@ public class HashTable {
 	 * @param word String of word to be deleted.
 	*/
 	public void delete(String word) {
-
+		word = word.toLowerCase();
+		int index = hash(word);
+		if (table[index] == null) {
+			return;
+		} else {
+			for (int i = 0; i < table[index].size(); i++) {
+				if (table[index].get(i).word.equals(word)) {
+					table[index].remove(i);
+				}
+			}
+			return;
+		}
 	}
 	/* 
 	 * This method must return the DataItem containing the word with the greatest occurrence count of all the words stored in the hash table.
 	*/
 	public DataItem highcount() {
-		return null;
+		DataItem biggest = new DataItem("");
+		biggest.count = 0;
+		for (int i = 0; i < table.length; i++) {
+			if (table[i] != null) {
+				for (int j = 0; j < table[i].size(); j++) {
+					if (table[i].get(j).count > biggest.count) {
+						biggest = table[i].get(j);
+					}
+				}
+			}
+		}
+		return biggest;
 	}
 	/*
 	 * This method will compute the average number of comparisons done for searches that were successful and return that value.
 	*/
 	public double get_avg_successful_search() {
 		double success = 0;
-		success = .5*(1+(1/(1-11067)));
+		int count = 0;
+		for (int i = 0; i < table.length; i++) {
+			if (table[i] != null) {
+				count++;
+			}
+		}
+		double load = count/11067.0;
+		success = 1 + (load/2);
 		return success;
 	}
 	/*
 	 * This method will compute the average number of comparisons done for searches that were unsuccessful and return that value.
 	*/
 	public double get_avg_unsuccessful_search() {
-		return 0.0;
+		//double unsuccess = 0;
+		int count = 0;
+		for (int i = 0; i < table.length; i++) {
+			if (table[i] != null) {
+				count++;
+			}
+		}
+		//System.out.println(count + "/" + 11067 + "=" + count/11067);
+		double load = count/11067.0;
+		return load;
 	}
 	private LinkedList<DataItem>[] table = new LinkedList[11067];
 	private int hash(String s) {
@@ -65,7 +119,7 @@ public class HashTable {
 			} else {
 				key += (3 * val);
 			}
-			System.out.println(val + " " + key);
+			//System.out.println(val + " " + key);
 		}
 		return (int)(key % (double) table.length);
 
