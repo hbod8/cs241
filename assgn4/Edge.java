c63dc96eccd8f86e24e8b52fc05d64ed978808ca
Harry Saliba <salibah@wwu.edu>
2018-12-04 20:33:23 -0800

    Finished Edge and Vertex contstrution.

--- assgn4/Edge.java
+++ assgn4/Edge.java
@@ -2,9 +2,17 @@
 
 
 public class Edge {
-
+    private Vertex town1, town2;
+    public final int distance;
     public Edge (Vertex town1, Vertex town2, int distance) {
-    
+    	this.town1 = town1;
+	this.town2 = town2;
+	this.distance = distance;
     }
-
+    public Vertex[] getVerticies() {
+	Vertex[] verts = new Vertex[2];
+	verts[0] = town1;
+	verts[1] = town2;
+	return verts;
+    }
 }
--- assgn4/Vertex.java
+++ assgn4/Vertex.java
@@ -5,18 +5,21 @@ public  class Vertex {
     public final String name;
     private LinkedList<Edge> edges;
     public Vertex (String town) {
-	this.name = name;
+	this.name = town;
     }
     public boolean addEdge(Edge e) {
 	edges.add(e);
+	return true;
     }
-    public boolean isAdjecent(Vertex v) {
+    public boolean isAdjacent(Vertex v) {
 	for (int i = 0; i < edges.size(); i++) {
 	    Edge e = edges.get(i);
-	    if (e.getVerticies()[0]
+	    if (e.getVerticies()[0].equals(v) || e.getVerticies()[1].equals(v))
+		return true;
 	}
+	return false;
     }
-    @Override
+    //@Override
     public boolean equals(Vertex v) {
 	return v.name.equals(this.name);
     }
